f1455dc2f817689daaa14fe57b9e006060892e55
Bassim Durzi <bassimdurzi@gmail.com>
2023-11-30 23:10:45 +0100

    Initial commit

---
+++ src/main/java/fortnoxcarrental/FortnoxCarRentalApplication.java
@@ -0,0 +1,13 @@
+package fortnoxcarrental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FortnoxCarRentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FortnoxCarRentalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fortnoxcarrental/FortnoxCarRentalApplicationTests.java
@@ -0,0 +1,13 @@
+package fortnoxcarrental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FortnoxCarRentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
